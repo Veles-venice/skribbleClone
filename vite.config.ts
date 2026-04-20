bdff7db8be64ca87f52f646bd8ffd3dcb64b0f73
Veles-venice <veniceveles@gmail.com>
2026-04-20 03:39:01 +0000

    changes in config

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,6 @@ export default defineConfig({
   },
   envDir: path.resolve(__dirname),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true,
-  },
+  outDir: "dist",
+}
 });
